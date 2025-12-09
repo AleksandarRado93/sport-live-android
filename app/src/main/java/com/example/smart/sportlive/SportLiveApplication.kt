14e1088c0d352e9f5c1c0c518dcd0b680230683f
Aleksandar <a.radovanovic@levi9.com>
2025-12-09 13:35:28 +0100

    chore: setup Hilt with network, cache and repository modules

---
+++ app/src/main/java/com/example/smart/sportlive/SportLiveApplication.kt
@@ -0,0 +1,8 @@
+package com.example.smart.sportlive
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SportLiveApplication : Application()
+
---
+++ app/src/main/java/com/example/smart/sportlive/di/NetworkModule.kt
@@ -0,0 +1,14 @@
+package com.example.smart.sportlive.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    // TODO: Add OkHttpClient provider
+    // TODO: Add Retrofit provider
+    // TODO: Add TodoApi provider
+}
+
